29946014c0f1605a457e73fcfacded603f867e34
sergey_tverye <stverye@gmail.com>
2023-08-26 22:39:12 +0300

    Refactoring for login mechanic

--- src/App.js
+++ src/App.js
@@ -1,57 +1,49 @@
+import { useState, useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
-import DayPage from './components/DayPage.js'
-import {  useState } from 'react';
-import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import DayPage from './components/DayPage';
 import { userManager } from './utils/localStorageDB';
 
-
 function App() {
-  // here should be token
-  // let [token, setToken] = useState()
-  let [userId, setUserId] = useState()
-  const navigate = useNavigate()
+  const [userId, setUserId] = useState(userManager.getCurrentUser());
+
+  useEffect(() => {
+    if (userId) {
+      userManager.setCurrentUser(userId);
+    }
+  }, [userId]);
 
   const exitEvent = () => {
-    console.log('exitEvent')
-    userManager.removeCurrentUser()
-    setUserId(undefined)
-    navigate('/')
-  }
+    userManager.removeCurrentUser();
+    setUserId(null);
+  };
 
   const loginEvent = (currentUserId) => {
-    console.log('loginEvent')
-    userManager.setCurrentUser(currentUserId)
-    setUserId(currentUserId)
-  }
-
-  const loginNav = () => {
-    console.log(`loginNav - ${userId}`)
-    
-    if (userId) {
-      return '/home'
-    }
-    let currentUserId = userManager.getCurrentUser()
-    if (currentUserId) {
-      setUserId(currentUserId)
-      return '/'
-    }
-    return '/login'
-  }
+    setUserId(currentUserId);
+  };
 
   return (
-    <div className="main-container">
+      <div className="main-container">
         <Routes>
-          <Route path="/" element={<Navigate to={loginNav()} />} />
-          <Route path="/login" element={<LoginPage loginEvent={loginEvent}/>} />
-          <Route path='/home' element={<HomePage exitEvent={exitEvent} />} >
-            <Route path='/home/' element={<Navigate to='/home/day/today' />} />
-            <Route path='/home/day/:date' element={<DayPage userID={userId}/>} />
-            <Route path='/home/stats' element={<div>Stats</div>} />
-          </Route>
+          {userId ? (
+              <>
+                <Route path="/" element={<Navigate to="/home/day/today" />} />
+                <Route path="/home/*" element={<HomePage exitEvent={exitEvent} />}>
+                  <Route index element={<Navigate to="/day/today" />} />
+                  <Route path="day/:date" element={<DayPage userID={userId} />} />
+                  <Route path="stats" element={<div>Stats</div>} />
+                </Route>
+              </>
+          ) : (
+              <>
+                <Route path="/" element={<Navigate to="/login" />} />
+                <Route path="/login" element={<LoginPage loginEvent={loginEvent} />} />
+              </>
+          )}
         </Routes>
-    </div>
+      </div>
   );
 }
 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,15 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet } from 'react-router-dom';
 
-function HomePage({exitEvent}) {
+function HomePage({ exitEvent }) {
     return (
         <div className="home-page">
             <button onClick={exitEvent}>Exit</button>
-            <Link to='/home/day/today'><button>Home</button></Link>
-            <Link to='/home/stats'><button>Stats</button></Link>
+            <Link to="/home/day/today"><button>Home</button></Link>
+            <Link to="/home/stats"><button>Stats</button></Link>
             <div>Home</div>
             <Outlet />
         </div>
-      );
+    );
 }
 
 export default HomePage;
